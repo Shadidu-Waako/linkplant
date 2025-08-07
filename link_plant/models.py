6c1ef835041c8f676d27cc84a54049707dd5bdb7
Waako Shadidu Ismail <shadiduwaako@gmail.com>
2025-08-07 10:03:37 +0300

    Added the profile and links table in the models file

--- link_plant/models.py
+++ link_plant/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+# Profile table -> Name, slug, bg_color
+class Profile(models.Model):
+    BG_CHOICES = (
+        ('blue', 'Blue'),
+        ('green', 'Green'),
+        ('red', 'Red'),
+        ('yellow', 'Yellow'),
+        ('purple', 'Purple'),
+        ('orange', 'Orange'),
+        ('pink', 'Pink'),
+        ('brown', 'Brown'),
+        ('gray', 'Gray'),
+        ('black', 'Black'),
+        ('white', 'White'),
+        ('cyan', 'Cyan'),
+        ('magenta', 'Magenta'),
+    )
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    bg_color = models.CharField(max_length=50, choices=BG_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+# Link table -> Text, url, profile
+class Links(models.Model):
+    text = models.CharField(max_length=100)
+    url = models.URLField(max_length=200)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='links')
